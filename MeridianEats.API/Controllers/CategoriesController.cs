b150edfb1520444e9ccb2d3354a1b1feaba19cc0
mkafzaluddin <mkafzaluddin.ec@gmail.com>
2026-04-23 04:46:08 -0500

    Fix categories API to include ImageUrl

--- MeridianEats.API/Controllers/CategoriesController.cs
+++ MeridianEats.API/Controllers/CategoriesController.cs
@@ -1,26 +1,24 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MeridianEats.API.Data;
-using MeridianEats.API.Helpers;
+
 namespace MeridianEats.API.Controllers;
+
 [ApiController]
 [Route("api/categories")]
 public class CategoriesController : ControllerBase
 {
     private readonly AppDbContext _db;
-
-    public CategoriesController(AppDbContext db)
-    {
-        _db = db;
-    }
+    public CategoriesController(AppDbContext db) => _db = db;
 
     [HttpGet]
-    public async Task<IActionResult> GetCategories()
-    {
-        var categories = await _db.Categories
-            .Select(c => new { c.Id, c.Name })
-            .ToListAsync();
-        return Ok(categories);
-    }
+    public async Task<IActionResult> GetAll() =>
+        Ok(await _db.Categories
+            .OrderBy(c => c.Id)
+            .Select(c => new {
+                c.Id,
+                c.Name,
+                c.ImageUrl  // ← add this
+            })
+            .ToListAsync());
 }
